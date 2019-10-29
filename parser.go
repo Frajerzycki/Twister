e77d6488f967a6240c4c104ddaa4e14c0a97d34e
BuildTools <frajerzycki@protonmail.com>
2019-10-29 22:32:30 +0100

    Refactor parser

--- parser.go
+++ parser.go
@@ -6,32 +6,29 @@ import (
 	"strconv"
 )
 
-func parseKeySize(index *int) (uint, error) {
+func getCommandLineArgument(index *int) string {
 	(*index)++
 	var argument string
 	if *index < len(os.Args) {
 		argument = os.Args[*index]
-		if size, err := strconv.Atoi(argument); err == nil {
-			return uint(size), nil
-		}
-	} else {
-		argument = ""
+	}
+	return argument
+}
+
+func parseKeySize(index *int) (uint, error) {
+	argument := getCommandLineArgument(index)
+	if size, err := strconv.Atoi(argument); err == nil {
+		return uint(size), nil
 	}
 	return 0, &intFormatError{argument, keySizeBase, "key size"}
 }
 
 func parseKey(index *int) (*big.Int, error) {
-	(*index)++
-	var argument string
+	argument := getCommandLineArgument(index)
+	key := new(big.Int)
 	var ok bool
-	if *index < len(os.Args) {
-		argument = os.Args[*index]
-		key := new(big.Int)
-		if key, ok = key.SetString(argument, keyBase); ok {
-			return key, nil
-		}
-	} else {
-		argument = ""
+	if key, ok = key.SetString(argument, keyBase); ok {
+		return key, nil
 	}
 	return nil, &intFormatError{argument, keyBase, "key"}
 }
