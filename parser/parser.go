57f71433fbe4c6e28627d2eb2f8ba1c842fdf830
BuildTools <frajerzycki@protonmail.com>
2019-11-01 22:51:41 +0100

    Add parsing whether some data is text or not

--- parser/parser.go
+++ parser/parser.go
@@ -1,8 +1,6 @@
 package parser
 
-import (
-	"os"
-)
+import "os"
 
 func ParseArguments(parameters *Arguments) error {
 	var err error
@@ -18,6 +16,16 @@ func ParseArguments(parameters *Arguments) error {
 			parameters.Key, err = parseKey(&index)
 		case "-kf":
 			parameters.Key, err = parseKeyFromFile(&index)
+		default:
+			if submatches := formatArgumentRegexp.FindStringSubmatch(argument); submatches != nil {
+				if submatches[2] == "i" {
+					parameters.IsInputDataText = submatches[3] == "d"
+					parameters.IsInputKeyText = submatches[3] == "k"
+				} else {
+					parameters.IsOutputDataText = submatches[3] == "d"
+					parameters.IsOutputKeyText = submatches[3] == "k"
+				}
+			}
 		}
 		if err != nil {
 			return err
