db5867bf14a407d860d736f61888d08797df70cd
BuildTools <frajerzycki@protonmail.com>
2019-11-06 18:00:56 +0100

    Remove unused error

--- parser/errors.go
+++ parser/errors.go
@@ -5,19 +5,8 @@ import (
 	"fmt"
 )
 
-type formatError struct {
-	representation string
-	base           int
-	name           string
-	dataType       string
-}
-
 type manyParameterValuesError struct {
 	parameterName string
-}
-
-func (err *formatError) Error() string {
-	return fmt.Sprintf("Wrong %v format, \"%v\" is not an %v in %v base.", err.name, err.representation, err.dataType, err.base)
 }
 
 func (err *manyParameterValuesError) Error() string {
