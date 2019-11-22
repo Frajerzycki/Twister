ae60ead566e7acdefd498c72e478b68f3dc9d691
ikcilrep <frajerzycki@protonmail.com>
2019-11-22 18:33:26 +0100

    Rename test to have package and function name in its name

---
+++ test/generation_test.go
@@ -0,0 +1,35 @@
+package test
+
+import (
+	"encoding/base64"
+	"github.com/ikcilrep/twister/internal/functionality"
+	"github.com/ikcilrep/twister/internal/parser"
+	"testing"
+)
+
+func Test_functionality_GenerateKey(t *testing.T) {
+	keyWriter := &TestingWriter{}
+	keyOutput := &parser.Output{Writer: keyWriter}
+	arguments := &parser.Arguments{KeyOutput: keyOutput}
+	for keySize := 0; keySize < 128; keySize++ {
+		arguments.KeySize = keySize
+		keyOutput.IsBinary = false
+		err := functionality.GenerateKey(arguments)
+		if err != nil {
+			t.Error(err)
+		}
+		decodedKeyBytes, err := base64.StdEncoding.DecodeString(string(keyWriter.data))
+		if len(decodedKeyBytes) != keySize {
+			t.Errorf("%v hasn't got length %v, but %v", keyWriter.data, keySize, len(keyWriter.data))
+		}
+
+		keyOutput.IsBinary = true
+		err = functionality.GenerateKey(arguments)
+		if err != nil {
+			t.Error(err)
+		}
+		if len(keyWriter.data) != keySize {
+			t.Errorf("%v hasn't got length %v, but %v", keyWriter.data, keySize, len(keyWriter.data))
+		}
+	}
+}
